ea74fc7b97e1e5129ca63f481f3aa154a18c6fbc
siksaka09 <earthsik09@gmail.com>
2022-09-15 14:48:57 +0700

    commit

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { comments } from "../libs/comments";
 import Reply from "./Reply";
 
 export default function Comment(props) {
+  const [filterText] = useState("");
+  const filtered = comments.filter((x) => x.username.includes(filterText));
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
-
+      {filtered.map((x) => (
+        <div className="d-flex gap-2 my-2">
+          <img
+            src={x.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#E5E7EB" }}
+          >
+            <span className="fw-semibold">{x.username}</span>
+            <br />
+            <span>{x.commentText}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{x.likeNum}</span>
+            </div>
+          </div>
+        </div>
+      ))}
       {/*render Reply here... */}
+      <Reply></Reply>
     </div>
   );
 }
